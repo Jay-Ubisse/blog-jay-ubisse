cb12621e8cea684408ca163501f1db8503723e53
Jay-Ubisse <joaquimubisse@gmail.com>
2023-10-15 22:04:33 +0200

    bug fix

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
+/*
 let prisma: PrismaClient;
 declare global {
     namespace NodeJS {
@@ -17,5 +18,19 @@ if(process.env.NODE_ENV === 'production') {
     }
     prisma = global.prisma;
 }
+*/
 
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
+}
 export default prisma;
