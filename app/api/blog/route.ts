84fe23c3c403ded29ef88e9efa857639bbd76ba5
Jay-Ubisse <joaquimubisse@gmail.com>
2024-12-11 22:07:19 +0200

    bug fixes

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,30 +1,18 @@
 import prisma from "@/prisma";
 import { NextResponse } from "next/server";
 
-export async function main() {
-  try {
-    await prisma.$connect();
-  } catch (error) {
-    return Error("Connection do DB failed.");
-  }
-}
-
 export const GET = async (req: Request, res: NextResponse) => {
   try {
-    await main();
     const posts = await prisma.post.findMany();
     return NextResponse.json({ message: "Success", posts }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
-  } finally {
-    await prisma.$disconnect();
   }
 };
 
 export const POST = async (req: Request, res: NextResponse) => {
   try {
     const { title, description, tags } = await req.json();
-    await main();
 
     const post = await prisma.post.create({
       data: { description, title, tags },
@@ -32,7 +20,5 @@ export const POST = async (req: Request, res: NextResponse) => {
     return NextResponse.json({ message: "Success", post }, { status: 201 });
   } catch (error) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
-  } finally {
-    await prisma.$disconnect();
   }
 };
